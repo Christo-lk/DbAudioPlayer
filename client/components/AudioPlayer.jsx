dc68d7efa5c7f57cd4b9132bca4ac5eb007c737d
Christo Karadjov <clkaradjov@gmail.com>
2021-03-18 11:41:50 +1300

    clicking button pauses and plays song

--- client/components/AudioPlayer.jsx
+++ client/components/AudioPlayer.jsx
@@ -1,16 +1,31 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 export default function AudioPlayer ({ tracks }) {
-  console.log(tracks)
-
   // useState
   const [trackIndex, setTrackIndex] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [playPause, setPlayPause] = useState(false)
 
   const { title, artist, audioSrc, image } = tracks[trackIndex]
 
   // useRefs
-
+  // Defines audio source
   const audio = useRef(new Audio(audioSrc))
+
+  // USE EFFECTS
+
+  useEffect(() => {
+    playPause ? audio.current.pause() : audio.current.play()
+  }, [playPause])
+
+  // FUNCTIONS
+
+  // handles play and stop playing
+  function clickPlayPause () {
+    playPause ? setPlayPause(false) : setPlayPause(true)
+
+    console.log(playPause)
+  }
 
   return (
     <>
@@ -18,7 +33,7 @@ export default function AudioPlayer ({ tracks }) {
         <img src={image} className="w-32"/>
         <h2>{title}</h2>
         <h2>{artist}</h2>
-        <button onClick={() => audio.current.play()}> play </button>
+        <button onClick={() => { clickPlayPause() }}> play </button>
       </div>
     </>
   )
