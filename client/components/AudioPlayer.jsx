8f437c6731aaa78301267714c22e60e5a52d0666
Christo Karadjov <clkaradjov@gmail.com>
2021-03-18 11:06:45 +1300

    destructured data and shows up correctly

--- client/components/AudioPlayer.jsx
+++ client/components/AudioPlayer.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function AudioPlayer ({ tracks }) {
   console.log(tracks)
+
+  // useState
+  const [trackIndex, setTrackIndex] = useState(0)
+
+  const {title, artist, audioSrc, image} = tracks[trackIndex]
+
+
   return (
     <>
-
+      <img src={image} className="w-32"/>
+      <h2>{title}</h2>
+      <h2>{artist}</h2>
     </>
   )
 }
