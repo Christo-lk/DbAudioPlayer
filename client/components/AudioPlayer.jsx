2cb85ccdac37c4c05819cb16eb9b25c3efa5fead
Christo Karadjov <clkaradjov@gmail.com>
2021-03-18 12:23:34 +1300

    play pause button finalised and works as it should

--- client/components/AudioPlayer.jsx
+++ client/components/AudioPlayer.jsx
@@ -1,31 +1,19 @@
 import React, { useState, useRef, useEffect } from 'react'
 
 export default function AudioPlayer ({ tracks }) {
-  // useState
   const [trackIndex, setTrackIndex] = useState(0)
   const [isPlaying, setIsPlaying] = useState(false)
-  const [playPause, setPlayPause] = useState(false)
 
   const { title, artist, audioSrc, image } = tracks[trackIndex]
 
-  // useRefs
+  console.log(isPlaying)
   // Defines audio source
   const audio = useRef(new Audio(audioSrc))
 
-  // USE EFFECTS
-
-  useEffect(() => {
-    playPause ? audio.current.pause() : audio.current.play()
-  }, [playPause])
-
-  // FUNCTIONS
-
   // handles play and stop playing
-  function clickPlayPause () {
-    playPause ? setPlayPause(false) : setPlayPause(true)
-
-    console.log(playPause)
-  }
+  useEffect(() => {
+    isPlaying ? audio.current.play() : audio.current.pause()
+  }, [isPlaying])
 
   return (
     <>
@@ -33,7 +21,9 @@ export default function AudioPlayer ({ tracks }) {
         <img src={image} className="w-32"/>
         <h2>{title}</h2>
         <h2>{artist}</h2>
-        <button onClick={() => { clickPlayPause() }}> play </button>
+
+        {isPlaying ? <button onClick={() => setIsPlaying(false)}> pause</button> : <button onClick={() => setIsPlaying(true)}> play </button>}
+
       </div>
     </>
   )
