4a20b25d62228dfe69bb85403536e5181bb547b6
Christo Karadjov <clkaradjov@gmail.com>
2021-03-25 18:44:58 +1300

    insert server route and db function work

--- server/routes/apiSongs.js
+++ server/routes/apiSongs.js
@@ -6,6 +6,7 @@ const router = express.Router()
 
 module.exports = router
 
+// RETURN ALL SONGS
 router.get('/getsongs', (req, res) => {
   db.getSongs()
     .then(result => res.json(result))
@@ -16,10 +17,28 @@ router.get('/getsongs', (req, res) => {
   // res.send('youve reached the api')
 })
 
+// RETURN INDIVIDUAL SONG
 router.get('/getindsong/:id', (req, res) => {
   const id = req.params.id
 
   db.getIndSong(id)
     .then(result => res.json(result))
+    .catch(err => console.log(err))
+})
+
+// ADD SONG
+router.post('/addsong', (req, res) => {
+  const song = {
+    title: 'test',
+    artist: 'christo',
+    image: './test',
+    audioSrc: './test'
+  }
+
+  db.addSong(song)
+    .then(result => {
+      res.status(201).json(result)
+      return null
+    })
     .catch(err => console.log(err))
 })
