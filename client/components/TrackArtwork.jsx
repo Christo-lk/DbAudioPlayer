de6b062430d55c13b4f308676fcacc4050154d46
Christo Karadjov <clkaradjov@gmail.com>
2021-04-12 18:38:13 +1200

    resize track title font

--- client/components/TrackArtwork.jsx
+++ client/components/TrackArtwork.jsx
@@ -38,9 +38,9 @@ function TrackArtwork (props) {
   return (
     <>
       <img src={imageSrc} className={imgClassSelector()}/>
-      <div className="flex flex-col justify-center my-1">
-        <h2 className="mx-auto">{title}</h2>
-        <h2 className="mx-auto italic text-sm">{artist}</h2>
+      <div className="flex flex-col justify-center my-1 truncate">
+        <h2 className="mx-auto truncate text-lg font-semibold">{title}</h2>
+        <h2 className="mx-auto italic">{artist}</h2>
       </div>
     </>
 
