bd0cfeebbfb0b382be9b6f57ee6e5a19bf636dbb
Christo Karadjov <clkaradjov@gmail.com>
2021-04-16 10:30:59 +1200

    liked track component correcly renders indTrack

--- client/components/LikedTracks.jsx
+++ client/components/LikedTracks.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import IndTrack from './IndTrack'
 
-function LikedTracks () {
+function LikedTracks ({ tracks }) {
+  const LikedTracks = tracks.filter(track => track.isLiked === 1)
+  console.log('likedtracks: ', LikedTracks)
+
   return (
     <>
+      {LikedTracks.map(track => <IndTrack key={track.id} track={track}/>)}
     </>
   )
 }
 
 function mapStateToProps (state) {
   return {
+    tracks: state.tracks
 
   }
 }
