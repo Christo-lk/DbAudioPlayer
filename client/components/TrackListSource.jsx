b3e7ed08dc03a6bc0e7aee10f9cb654bfa54e4b6
Christo Karadjov <clkaradjov@gmail.com>
2021-04-16 17:59:05 +1200

    tidy tracklistsource component

--- client/components/TrackListSource.jsx
+++ client/components/TrackListSource.jsx
@@ -2,39 +2,12 @@ import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 
 // COMPONENTS
-import TrackList2 from './TrackList2'
 import AddSongForm from './AddSongForm'
 import LikedTracks from './LikedTracks'
 import AllTracks from './AllTracks'
 
 function TrackListSource ({ trackListSource }) {
-  console.log('TL sourrce: ', trackListSource)
-
-  // const [component, setComponent] = useState()
-
-  // console.log('component: ', component)
-
-  // useEffect(() => {
-  //   renderComponent()
-  // }, [trackListSource])
-
-  // function renderComponent (trackListSource) {
-  //   switch (trackListSource) {
-  //     case 'TRACKS':
-  //       return () => setComponent(<TrackList2/>)
-
-  //     case 'LIKED_SONGS':
-  //       return () => setComponent(<LikedTracks/>)
-
-  //     case 'ADD_SONG_FORM':
-  //       return () => setComponent(<AddSongForm/>)
-
-  //     default:
-  //       return () => setComponent(<TrackList2/>)
-  //   }
-  // }
-
-  function switchTest () {
+  function componentSource () {
     switch (trackListSource) {
       case 'ALL_SONGS':
         return <AllTracks/>
@@ -52,15 +25,14 @@ function TrackListSource ({ trackListSource }) {
 
   return (
     <>
-      {switchTest()}
+      {componentSource()}
     </>
   )
 }
 
 function mapStateToProps (state) {
   return {
-    trackListSource: state.trackListSource,
-    tracks: state.tracks
+    trackListSource: state.trackListSource
   }
 }
 
