ff4e5ea583f6ca1b799b778c65bfca1e74abbf19
Christo Karadjov <clkaradjov@gmail.com>
2021-04-17 15:55:41 +1200

    re styled tracklist nav

--- client/components/TrackListNav.jsx
+++ client/components/TrackListNav.jsx
@@ -8,13 +8,13 @@ import { connect } from 'react-redux'
 function TrackListNav ({ trackListSource }) {
   return (
     <>
-      <div className="  h-auto bg-blue-400 flex items-center flex-row sticky pt-1 pb-4 pl-1 ">
+      <div className="  h-auto bg-blue-400 flex items-center flex-row sticky pt-1 pb-3 pl-1 ">
         <div className="flex flex-row">
-          <div onClick={() => store.dispatch(setTrackListSource('ALL_TRACKS'))} className=" h-12 w-28 bg-yellow-400 mr-2">
-            <h2 className="text-2xl leading-tight font-bold hover:text-white">All Tracks:</h2>
+          <div onClick={() => store.dispatch(setTrackListSource('ALL_TRACKS'))} className="h-5 w-20 bg-yellow-400 mr-4">
+            <h2 className=" ml-1 mt-1 mr-2 text-2xl leading-tight font-bold hover:text-white">Library:</h2>
           </div>
-          <div onClick={() => store.dispatch(setTrackListSource('LIKED_TRACKS'))} className="h-12 w-28 bg-yellow-400 mr-2">
-            <h2 className="text-2xl leading-tight font-bold hover:text-white" >Liked Tracks:</h2>
+          <div onClick={() => store.dispatch(setTrackListSource('LIKED_TRACKS'))} className="ml-5 h-5 w-14 bg-yellow-400 mr-4">
+            <h2 className="ml-1 mt-1 text-2xl leading-tight font-bold hover:text-white" >Liked:</h2>
           </div>
           <div onClick={() => store.dispatch(setTrackListSource('ADD_SONG_FORM'))} className="h-7 w-7 flex-center bg-yellow-400 mx-2 ml-16">
             <button className="text-4xl hover:text-white">+</button>
