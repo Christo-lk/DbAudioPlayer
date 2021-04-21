793982dddad2faf48e0d467a2da43d0ee4acfd00
Christo Karadjov <clkaradjov@gmail.com>
2021-04-21 21:34:17 +1200

    getIndSong db function Written

--- server/db/dbSongs.test.js
+++ server/db/dbSongs.test.js
@@ -12,11 +12,23 @@ beforeEach(() => {
   return testDb.seed.run()
 })
 
-describe('getSongs', () => {
+describe('getSongs()', () => {
   it('returns all songs', () => {
     return db.getSongs(testDb)
       .then(result => {
-        expect(result).toHaveLength(8)
+        expect(result).toHaveLength(9)
+        return null
+      })
+  })
+})
+
+describe('GetIndSong()', () => {
+  it('returns individual songby id', () => {
+    return db.getIndSong(1, testDb)
+      .then(result => {
+        expect(result.id).toBe(1)
+        expect(result.title).toBe('We Are The People')
+        expect(result.artist).toBe('Empire Of The Sun')
         return null
       })
   })
