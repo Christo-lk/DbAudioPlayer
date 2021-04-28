a34c28aa2dfc72cf862b9b268b6929aaf03af88a
Christo Karadjov <clkaradjov@gmail.com>
2021-04-29 11:04:10 +1200

    alphabetic order and reverse alphabetic order work

--- client/components/AllTracks.jsx
+++ client/components/AllTracks.jsx
@@ -3,12 +3,49 @@ import { connect } from 'react-redux'
 
 import IndTrack from './IndTrack'
 
-function AllTracks ({ tracks, selectedTrackId }) {
+function AllTracks ({ tracks, selectedTrackId, trackOrder }) {
+  const alphabeticTracks = tracks.sort((a, b) => {
+    if (a.title < b.title) {
+      return -1
+    } if (a.title > b.title) {
+      return 1
+    } else { return 0 }
+  })
+
+  console.log('TO: ', trackOrder)
+
+  function setTrackOrder () {
+    switch (trackOrder) {
+      case 'DEFAULT':
+        return tracks.map(track =>
+          <IndTrack track={track} key={track.id} />
+        )
+
+      case 'ALPHABETIC':
+        return alphabeticTracks.map(track =>
+          <IndTrack track={track} key={track.id} />
+        )
+
+      case 'REVERSE_ALPHABETIC':
+        return alphabeticTracks.reverse().map(track =>
+          <IndTrack track={track} key={track.id} />
+        )
+
+      default:
+        return tracks.map(track =>
+          <IndTrack track={track} key={track.id} />
+        )
+    }
+  }
+
   return (
     <>
-      {tracks.map(track =>
+
+      {/* {tracks.map(track =>
         <IndTrack track={track} key={track.id} />
-      )}
+      )} */}
+
+      {setTrackOrder()}
     </>
   )
 }
@@ -16,7 +53,8 @@ function AllTracks ({ tracks, selectedTrackId }) {
 function mapStateToProps (state) {
   return {
     tracks: state.tracks,
-    selectedTrackId: state.selectedTrack.id
+    selectedTrackId: state.selectedTrack.id,
+    trackOrder: state.trackOrder
   }
 }
 
--- client/redux/actions/selectedTrack.js
+++ client/redux/actions/selectedTrack.js
@@ -1,14 +1,8 @@
-const getSongs = require('../../api/songsApi')
-
 export function selectedTrack (track) {
-  // const { id, title, audioSrc, image } = track
-
   return {
     type: 'SELECTED_TRACK',
     track: track
-    // title,
-    // audioSrc,
-    // image
+
   }
 }
 
