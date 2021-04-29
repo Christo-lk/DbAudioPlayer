49abca85df033c73ef873e169d419b8f75c4a74b
Christo Karadjov <clkaradjov@gmail.com>
2021-04-29 21:28:48 +1200

    redux array correctly adds new object containing queed track

--- client/redux/reducers/queuedTracks.js
+++ client/redux/reducers/queuedTracks.js
@@ -1,10 +1,10 @@
 export default function queuedTracks (state = [], action) {
   switch (action.type) {
     case 'ADD_QUEUED_TRACK':
-      return {
+      return [
         ...state,
-        track: action.track
-      }
+        action.track
+      ]
 
     default:
       return state
