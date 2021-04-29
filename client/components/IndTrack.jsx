e9075bd23791d6918431e96e7247c20bcb0af4d6
Christo Karadjov <clkaradjov@gmail.com>
2021-04-29 21:40:49 +1200

    indtrack renders - symbol when tracklist is queued tracks

--- client/components/IndTrack.jsx
+++ client/components/IndTrack.jsx
@@ -16,6 +16,7 @@ import Delete from '../icons/delete.svg'
 import heartEmpty from '../icons/heartEmpty.svg'
 import heartFull from '../icons/heartFull.svg'
 import Add from '../icons/add.svg'
+import Remove from '../icons/remove.svg'
 
 function IndTrack ({ track, selectedTrack, showDeleteButton, trackListSource }) {
   // de structure props out of
@@ -101,7 +102,7 @@ function IndTrack ({ track, selectedTrack, showDeleteButton, trackListSource }) 
 
         <div className="flex items-center absolute right-5">
           <button onClick={() => isLikedHandler()} className="w-5 mr-2">{isLiked ? <img className={isLiked && trackListSource === 'LIKED_TRACKS' ? `opacity-80 hover:opacity-40` : 'opacity-80'} src={heartFull}/> : <img className="opacity-50 hover:opacity-80" src={heartEmpty}/>}</button>
-          <button onClick={()=> queuedHandler()}><img className="w-5 opacity-40 hover:opacity-80" src={Add}/></button>
+          <button onClick={()=> queuedHandler()}><img className="w-5 opacity-40 hover:opacity-80" src={trackListSource === 'QUEUED_TRACKS' ? Remove : Add}/></button>
           <button onClick={() => deleteHandler()}><img className={`${showDeleteButton ? 'block' : 'hidden'} w-4 opacity-20 hover:opacity-60`}src={Delete}/></button>
         </div>
       </div>
